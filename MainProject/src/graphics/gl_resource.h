aca18f9d56aea8cc882ca0f3e779e895a36f8d22
Dominic Fischer <dominicfischer7@gmail.com>
2017-10-17 18:22:45 +0100

    Deleted copy constructor and operator.

--- MainProject/src/graphics/gl_resource.h
+++ MainProject/src/graphics/gl_resource.h
@@ -4,6 +4,9 @@
 class GLResource
 {
 public:
+	GLResource() = default;
+	GLResource(const GLResource&) = delete;
+	GLResource& operator=(const GLResource&) = delete;
 	virtual ~GLResource() = default;
 	virtual GLuint getId() const = 0;
 };
